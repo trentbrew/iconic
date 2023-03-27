a690f5c5ce75503d01941ef0c8b471aacbe5a113
Trent Brew <tbrew1023@gmail.com>
2023-03-27 14:33:34 -0500

    add icon: bell_alt

--- hooks/api.js
+++ hooks/api.js
@@ -2,6 +2,8 @@ import icons from '@/data/icons'
 
 export default {
   icons: {
-    get: (name) => name ? icons.filter((icon) => icon.name == name)[0] : icons,
-  }
+    get: name => {
+      return name ? icons.filter(icon => icon.name == name)[0] : icons
+    },
+  },
 }
